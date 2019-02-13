0cd25591f6781704af86d18dec43eb656d74c9a3
Joonas Harjukallio <harjukallio@gmail.com>
2019-02-13 08:29:32 +0200

    Fix index names

--- db/migrate/20190213062651_create_join_table_campaigns_product_locations.rb
+++ db/migrate/20190213062651_create_join_table_campaigns_product_locations.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableCampaignsProductLocations < ActiveRecord::Migration[5.2]
   def change
     create_join_table :campaigns, :product_locations do |t|
-      t.index [:campaign_id, :product_location_id]
-      t.index [:product_location_id, :campaign_id]
+      t.index [:campaign_id, :product_location_id], name: 'campaign_id_pl_id'
+      t.index [:product_location_id, :campaign_id], name: 'pl_id_campaign_id'
     end
   end
 end
