501ce0deeeb3a2a1fd8330a10401bac3eb9cd6f7
Joonas Harjukallio <harjukallio@gmail.com>
2019-02-13 08:31:29 +0200

    Add CampaignController

---
+++ app/controllers/campaigns_controller.rb
@@ -0,0 +1,5 @@
+class CampaignsController < ApplicationController
+  def campaign_data
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'campaigns/campaign_data', to: 'campaings#campaign_data'
 end
