66eb439fa9627c6e932df4d41aa039cf5a56d299
Joonas Harjukallio <harjukallio@gmail.com>
2019-02-14 10:35:08 +0200

    Lets go with more simple one

--- app/data_fetchers/campaign_data.rb
+++ app/data_fetchers/campaign_data.rb
@@ -4,8 +4,6 @@ module CampaignData
       .visible(hidden_campaign_classes)
       .touches_time_range(start_date, end_date)
       .active
-      .select('campaigns.start_date, campaigns.end_date, campaigns.campaign_class, group_concat(DISTINCT campaigns.name)')
-      .group('campaigns.start_date, campaigns.end_date, campaigns.campaign_class')
-      .to_a
+      .distinct
   end
 end
